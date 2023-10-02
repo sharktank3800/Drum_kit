771343374536293213c01f4755b184f769f11f8d
[Ayman Shiham] <sharktank38000@gmail.com>
2023-10-01 21:43:47 -0400

    added switch statement for identifying which sound to play

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,43 @@
 var eachClick = document.querySelectorAll(".drum");
 
-function clickBtn(){
-    alert("yes i still click well done");
-};
-
-
 for(var i = 0; i < eachClick.length; i++){
     eachClick[i].addEventListener("click", clickBtn);
 };
 
+function clickBtn(){
 
+    var btnHtml = this.innerHTML;
 
+    switch (btnHtml) {
+        case "w":
+            var audio1 = new Audio("./assets/sounds/tom-1.mp3");
+            audio1.play();
+            break;
+        case "a":
+            var audio2 = new Audio("./assets/sounds/tom-2.mp3");
+            audio2.play();
+            break;
+        case "s":
+            var audio3 = new Audio("./assets/sounds/tom-3.mp3");
+            audio3.play();
+            break;
+        case "d":
+            var audio4 = new Audio("./assets/sounds/tom-4.mp3");
+            audio4.play();
+            break;
+        case "j":
+            var audio5 = new Audio("./assets/sounds/snare.mp3");
+            audio5.play();
+            break;
+        case "k":
+            var audio6 = new Audio("./assets/sounds/crash.mp3");
+            audio6.play();
+            break;
+        case "l":
+            var audio7 = new Audio("./assets/sounds/kick-bass.mp3");
+            audio7.play();
+            break;
+    }
+
+}
+   
